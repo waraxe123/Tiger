5ce5555d7a8a4ed3379aa272c548b988e1c8bf69
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-15 22:12:51 +0700

    Update ytd.py

--- TigerX/lib/ytd.py
+++ TigerX/lib/ytd.py
@@ -3,18 +3,22 @@ from io import BytesIO
 import math
 import time
 import traceback
+import asyncio
+import requests
+import wget
+from youtubesearchpython import SearchVideos
 from datetime import datetime as dt
 from pyrogram import filters, errors
 from pyrogram.types import Message
 from config import MAX_MESSAGE_LENGTH
 from pykillerx.helper.dl_helpers import progress_for_pyrogram
-# from youtube_dl import YoutubeDL, utils
 from yt_dlp import YoutubeDL, utils
 from pykillerx.helper.check_size import get_directory_size
 from TigerX import *
 from TigerX.lib import *
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
+
 
 ydl_search_opts = {
     "quiet": True,
@@ -275,3 +279,59 @@ async def ytp_dl(c, m):
                     caption=f"Download Errors!",
                 )
     return
+
+
+async def yt_video(c, m):
+    input_st = m.text
+    input_str= input_st.split(" ", 1)[1]
+    pablo = await m.edit_text("`Processing...`")
+    if not input_str:
+        await m.edit_text(
+            "`Please Give Me A Valid Input. You Can Check Help Menu To Know More!`"
+        )
+        return
+    await m.edit_text(f"`Searching {input_str} From Youtube. Please Wait.`")
+    search = SearchVideos(str(input_str), offset=1, mode="dict", max_results=1)
+    rt = search.result()
+    result_s = rt["search_result"]
+    url = result_s[0]["link"]
+    vid_title = result_s[0]["title"]
+    yt_id = result_s[0]["id"]
+    uploade_r = result_s[0]["channel"]
+    thumb_url = f"https://img.youtube.com/vi/{yt_id}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    downloaded_thumb = wget.download(thumb_url)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url, download=True)
+    except Exception as e:
+        await m.edit_text(f"**Failed To Download** \n**Error :** `{str(e)}`")
+        return
+    c_time = time.time()
+    file_path= f"{ytdl_data['id']}.mp4"
+    capy = f"**Video Name ➠** `{vid_title}` \n**Requested For ➠** `{input_str}` \n**Channel ➠** `{uploade_r}` \n**Link ➠** `{url}`"
+    await c.send_video(
+        m.chat.id,
+        video=open(file_path, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=downloaded_thumb,
+        caption=capy,
+        supports_streaming=True,
+    )
+    await m.delete()
+    for files in (downloaded_thumb, file_path):
+        if files and os.path.exists(files):
+            os.remove(files)
