eb36c7ab3eedd75c03b91956f225529e778ea26f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-15 16:37:48 +0700

    add modules upload file

---
+++ TigerX/lib/upd.py
@@ -0,0 +1,22 @@
+# this upload file 
+
+import asyncio
+import humanize
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from TigerX import *
+from TigerX.lib import *
+
+async def progress_callback(current, total, bot: Client, message: Message):
+    if int((current / total) * 100) % 25 == 0:
+        await message.edit(f"{humanize.naturalsize(current)} / {humanize.naturalsize(total)}")
+
+
+async def upload_helper(bot: Client, message: Message):
+    if len(message.command) > 1:
+        await bot.send_document('self', message.command[1], progress=progress_callback, progress_args=(bot, message))
+    else:
+        await message.edit('No path provided.')
+        await asyncio.sleep(3)
+    await message.delete()
