c5db05f1fb4401762ad9494b56a6b3657cc6b363
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-15 21:32:27 +0700

    Update csur.py

--- TigerX/lib/csur.py
+++ TigerX/lib/csur.py
@@ -9,38 +9,6 @@ from . import *
 
 __PLUGIN__ = os.path.basename(__file__.replace(".py", ""))
 
-
-async def extract_user_peler(message: Message) -> (int, str):
-    """extracts the user from a message"""
-    user_id = None
-    user_first_name = None
-
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        user_first_name = message.reply_to_message.from_user.first_name
-
-    elif len(message.command) > 1:
-        if len(message.entities) > 1:
-            required_entity = message.entities[1]
-            if required_entity.type == "text_mention":
-                user_id = required_entity.user.id
-                user_first_name = required_entity.user.first_name
-            elif required_entity.type == "mention":
-                user_id = message.text[
-                    required_entity.offset : required_entity.offset
-                    + required_entity.length
-                ]
-                user_first_name = user_id
-        else:
-            user_id = message.command[1]
-            user_first_name = user_id
-
-    else:
-        user_id = message.from_user.id
-        user_first_name = message.from_user.first_name
-
-    return (user_id, user_first_name)
-
 async def cas_check(c, m):
     if m.reply_to_message:
         user_id = m.reply_to_message.from_user.id
