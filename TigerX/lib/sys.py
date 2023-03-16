f3512a1eee7b0df4f870c595f2a3a2c0eba6ba90
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-16 11:09:43 +0700

    Create sys.py

---
+++ TigerX/lib/sys.py
@@ -0,0 +1,13 @@
+from TigerX import *
+from TigerX.lib import *
+
+async def sysinfo(c, m):
+    chat_id = m.chat.id
+    pro = await m.reply_text("`Prossing....`")
+    try:
+        sysinfo = (await shell_exec("neofetch | sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g'"))[0]
+        carbon = await make_carbon(sysinfo)
+        await c.send_photo(chat_id, carbon, caption=f"Carbonised by {c.me.mention}")
+        await pro.delete()
+    except Exception:
+        pass
