154846d5afb56d8e7d449add1b0e99ae6db9847c
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-17 01:32:20 +0700

    Update sang.py

--- TigerX/lib/sang.py
+++ TigerX/lib/sang.py
@@ -7,20 +7,20 @@ from TigerX.lib import *
 
 async def sangmata_check(client, message):
     args = await extract_user(message)
-    lol = await message.edit_text("`Processing...`")
+    lol = await message.reply_text("`Processing...`")
     if args:
         try:
             user = await client.get_users(args)
         except Exception:
-            return await lol.edit(f"`Please specify a valid user!`")
+            await m.reply_text(f"`Please specify a valid user!`")
+            return
     bot = "@SangMata_beta_bot"
     await client.send_message(bot, f"/search_id {user.id}")
-    await asyncio.sleep(1)
-    async for stalk in client.search_messages(bot, limit=2):
+    await asyncio.sleep(2)
+    async for stalk in client.search_messages(bot, limit=1):
         if not stalk:
-            await message.edit_text("**this person has never changed his name**")
+            await lol.edit_text("**this person has never changed his name**")
             return
         elif stalk:
             await message.edit(stalk.text)
-            await stalk.delete()
             await lol.delete()
