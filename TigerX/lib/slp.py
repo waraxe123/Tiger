9a3ffb188f22f69b8bcc9daf5f7c253239861fad
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-17 00:30:09 +0700

    Update slp.py

--- TigerX/lib/slp.py
+++ TigerX/lib/slp.py
@@ -328,7 +328,7 @@ async def slap_template_ok(c, m):
         hit = choice(HIT)
         throw = choice(THROW)
         response_message = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
-        await m.reply_text(response_message)
+        await c.send_message(m.chat.id, response_message)
     else:
         await message.reply_text("Please reply to a message to slap someone!")
 
