32e9730e5e33a2b1a28770d51827f3844f37622c
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-17 01:08:59 +0700

    Create sang.py

---
+++ TigerX/lib/sang.py
@@ -0,0 +1,30 @@
+import asyncio 
+from pykillerx.helper.basic import *
+from pykillerx.helper.hacking import *
+from pykillerx.helper import *
+from TigerX import *
+from TigerX.lib import *
+
+async def sangmata_check(client, message):
+    args = await extract_user(message)
+    lol = await message.edit_text("`Processing...`")
+    if args:
+        try:
+            user = await client.get_users(args)
+        except Exception:
+            return await lol.edit(f"`Please specify a valid user!`")
+    bot = "@SangMata_beta_bot"
+    try:
+        await client.send_message(bot, f"/search_id {user.id}")
+    except YouBlockedUser:
+        await client.unblock_user(bot)
+        await client.send_message(bot, f"/search_id {user.id}")
+    await asyncio.sleep(3)
+
+    async for stalk in client.search_messages(bot, query=f"History for {user.id}", limit=1):
+        if not stalk:
+            await message.edit_text("**this person has never changed his name**")
+            return
+        elif stalk:
+            await message.edit(stalk.text)
+            await stalk.delete()
