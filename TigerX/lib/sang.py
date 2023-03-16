6322d94306c03e25dea7267b5963b8b1320e2807
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-17 02:21:27 +0700

    Update sang.py

--- TigerX/lib/sang.py
+++ TigerX/lib/sang.py
@@ -6,21 +6,29 @@ from TigerX import *
 from TigerX.lib import *
 
 async def sangmata_check(client, message):
-    args = await extract_user(message)
-    lol = await message.reply_text("`Processing...`")
-    if args:
+    if message.reply_to_message and message.reply_to_message.from_user:
+        user_id = message.reply_to_message.from_user.id
+        args = message.text
         try:
             user = await client.get_users(args)
         except Exception:
             await message.reply_text(f"`Please specify a valid user!`")
             return
     bot = "@SangMata_beta_bot"
-    await client.send_message(bot, f"/search_id {user.id}")
+    if user_id:
+        await client.send_message(bot, f"/search_id {user_id}")
+    elif user:
+        await client.send_message(bot, f"/search_id {user}")
+    else: 
+        return 
     await asyncio.sleep(2)
-    async for stalk in client.search_messages(bot, limit=1):
-        if not stalk:
-            await message.reply_text("**this person has never changed his name**")
-            return
-        elif stalk:
-            await client.send_message(message.chat.id, stalk.text, reply_to_message_id=message.id)
-    await lol.delete()
+    try:
+        async for stalk in client.search_messages(bot, limit=1, timeout=5):
+            if not stalk:
+                await message.reply_text("**this person has never changed their name**")
+                return
+            elif stalk:
+                await client.send_message(message.chat.id, stalk.text, reply_to_message_id=message.id)
+                return
+    except asyncio.TimeoutError:
+        await message.reply_text("**Timed out while searching for messages**")
