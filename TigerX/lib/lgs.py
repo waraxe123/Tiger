533ddd86b74510e363968f0edfa46e148341331f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-18 16:28:25 +0700

    Create lgs.py

---
+++ TigerX/lib/lgs.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2020-2023 TeamKillerX <https://github.com/TeamKillerX>
+#
+# This file is part of TeamKillerX project,
+# and licensed under GNU Affero General Public License v3.
+# See the GNU Affero General Public License for more details.
+#
+# All rights reserved. See COPYING, AUTHORS.
+#
+
+import requests
+from PIL import Image, ImageDraw, ImageFont
+import glob
+from random import choice 
+from TigerX import *
+from TigerX.lib import *
+
+font_path = choice(glob.glob("/resources/fonts/*.ttf"))
+font_size = 60
+font_color = (255, 255, 255)
+
+URL_IMAGE = [
+   "https://github.com/TeamkillerX/TigerX-Userbot/raw/test/resources/image/hd2.jpg",
+   "https://github.com/TeamkillerX/TigerX-Userbot/raw/test/resources/image/images.jpeg",
+   "https://github.com/TeamkillerX/TigerX-Userbot/raw/test/resources/image/peakpx.jpg",
+
+]
+
+async def logo_write(client, message):
+    text = message.text.split(".logo_write ", 1)[1]
+    random_url = choice(URL_IMAGE)
+    response = requests.get(random_url)
+    img = Image.open(BytesIO(response.content))
+    font = ImageFont.truetype(font_path, font_size)
+
+    draw = ImageDraw.Draw(img)
+    text_size = draw.textsize(text, font)
+    text_position = ((img.width - text_size[0]) // 2, (img.height - text_size[1]) // 2)
+    draw.text(text_position, text, font=font, fill=font_color)
+
+    img.save("logo_with_text.png")
+    await client.send_photo(chat_id=message.chat.id, photo="logo_with_text.png")
