7a5822e4e923a4b44219eb780f503a826df63910
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-18 23:23:39 +0700

    Update truth_and_dare.py

--- TigerX/modules/plugins/truth_and_dare.py
+++ TigerX/modules/plugins/truth_and_dare.py
@@ -1,11 +1,11 @@
 from TigerX import *
-from TigerX.lib import *
+from TigerX.lib.slp import truth_string_str, dare_string_str_2
 
 @randydev(command("truth", cmd) & owner)
 async def truth_command(c: Client, m: Message):
-    await truth_string(c, m)
+    await truth_string_str(c, m)
 
 @randydev(command("dare", cmd) & owner)
 async def dare_command(c: Client, m: Message):
-    await dare_string(c, m)
+    await dare_string_str_2(c, m)
 
