08fcb64387bffcd05a4d0cc6a95cf417d70db91e
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-18 11:39:51 +0700

    Update sangmata.py

--- TigerX/modules/plugins/sangmata.py
+++ TigerX/modules/plugins/sangmata.py
@@ -1,6 +1,15 @@
 from TigerX import *
 from TigerX.lib import *
 
+from pykillerx.help import add_command_help
+
 @randydev(command(["sg", "sangmata"], cmd) & owner)
 async def sangmata_command(client: Client, message: Message):
     await sangmata_check(client, message)
+
+add_command_help(
+    "sangmata",
+    [
+        [f"sg [reply/userid/username]", "Its help uh to find someone name history."],
+    ],
+)
