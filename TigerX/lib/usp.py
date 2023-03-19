e5827fa6d13b781ca99f0e751b6b1be98e315261
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-19 15:25:30 +0700

    Update usp.py

--- TigerX/lib/usp.py
+++ TigerX/lib/usp.py
@@ -5,8 +5,12 @@ async def user_premium(c, m):
     out = ""
     async for mr in c.get_chat_members(m.chat.id):
         if mr.user.is_premium:
-             out+= mr.user.mention + "\n"
+             out+= mr.user.mention + "\n" 
         else:
              await m.reply_text("no user premium")
              return
-        await m.reply_text(out) #
+        try: 
+             await m.reply_text(out)
+         except Exception as e:
+             await m.reply_text(f"{e}")
+             return    
