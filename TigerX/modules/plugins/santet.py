d8446d876bd41726ab3701a092a27a9f6f081846
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-19 20:17:58 +0700

    Update santet.py

--- TigerX/modules/plugins/santet.py
+++ TigerX/modules/plugins/santet.py
@@ -4,3 +4,7 @@ from TigerX.lib import *
 @randydev(command("santet", cmd) & owner)
 async def santet_lu_fix(client: Client, message: Message):
     await typewriter(client, message)
+
+@randydev(command("whatsapp", cmd) & owner)
+async def wa_lu_fix(client: Client, message: Message):
+    await _whatsapp_hack(client, message)
