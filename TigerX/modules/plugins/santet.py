0c1cb5ce0d812fbdb9f0f21104d5589533f297e8
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-19 18:22:45 +0700

    Create santet.py

---
+++ TigerX/modules/plugins/santet.py
@@ -0,0 +1,6 @@
+from TigerX import *
+from TigerX.lib import *
+
+@randydev(command("santet", cmd) & owner)
+async def santet_lu_fix(client: Client, message: Message):
+    await typewriter(client, message)
