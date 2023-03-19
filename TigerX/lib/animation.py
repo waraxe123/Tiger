51a38afb0aa8091c2eb32f368671ba39e0be7966
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-19 20:10:48 +0700

    Update animation.py

--- TigerX/lib/animation.py
+++ TigerX/lib/animation.py
@@ -1,4 +1,5 @@
 from time import sleep
+import asyncio
 
 async def typewriter(client, message):
     typew = await message.reply_text("your prossing...")
@@ -310,3 +311,32 @@ async def typewriter(client, message):
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
     await typew.edit("`Target Berhasil Tersantet Online:v`")
+
+
+async def _whatsapp_hack(client, message):
+    if message.forward_from:
+        return
+
+    animation_interval = 2
+    animation_ttl = range(0, 15)
+    animation_chars = [
+        "Looking for WhatsApp databases in targeted person...",
+        " User online: True\nTelegram access: True\nRead Storage: True ",
+        "Hacking... 0%\n[░░░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 20s",
+        "Hacking... 11.07%\n[██░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 18s",
+        "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 16s",
+        "Hacking... 34.42%\n[█████░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 14s",
+        "Hacking... 42.17%\n[███████░░░░░░░░░░░░░]\n`Searching for databases`\nETA: 0m, 12s",
+        "Hacking... 55.30%\n[█████████░░░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 10s",
+        "Hacking... 64.86%\n[███████████░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 08s",
+        "Hacking... 74.02%\n[█████████████░░░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 06s",
+        "Hacking... 86.21%\n[███████████████░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 04s",
+        "Hacking... 93.50%\n[█████████████████░░░]\n`Decryption successful!`\nETA: 0m, 02s",
+        "Hacking... 100%\n[████████████████████]\n`Scanning file...`\nETA: 0m, 00s",
+        "Hacking complete!\nUploading file...",
+        "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12`"]
+
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await message.edit(animation_chars[i % 15])
+
