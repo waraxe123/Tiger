26d61c0fe54233144d2f83e4ee13dbd85182881e
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-20 08:32:39 +0700

    Create txc.py

---
+++ TigerX/lib/txc.py
@@ -0,0 +1,88 @@
+# credits @xtsea
+
+async def toxic_1(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**WAR WAR PALAK BAPAK KAU WAR, SOK KERAS BANGET GOBLOK DI TONGKRONGAN JADI BABU DI TELE SOK JAGOAN.**")
+    
+async def toxic_2(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**DIHH NAJISS ANAK HARAM LO GOBLOK JANGAN BELAGU DIMARI KAGA KEREN LU KEK BEGITU TOLOL.**")
+   
+async def toxic_3(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**MUKA BAPAK LU KEK KEPALA SAWIT ANJING, GA USAH NGATAIN ORANG, MUKA LU AJA KEK GEMBEL TEXAS GOBLOK!!.**")
+
+async def toxic_4(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**SOKAB BET LU GOBLOK, KAGA ADA ISTILAH NYA BAWAHAN TEMENAN AMA BOS!!.**")
+
+async def toxic_5(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**MATI AJA LU GOBLOK GAGUNA LU HIDUP DI BUMI.**")
+
+async def toxic_6(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**NAMANYA JUGA JAMET CAPER SANA SINI BUAT CARI NAMA.**")
+    
+async def toxic_7(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**CUIIHHH, MAKAN AJA MASIH NGEMIS LO GOBLOK, JANGAN SO NINGGI YA KONTOL GA KEREN LU KEK GITU GOBLOK.**")
+
+async def toxic_8(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**WOI LU SEMUA BABI😠.**")
+
+async def toxic_9(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**WOI ANJING, DENGER YA, ORANG GAK USAH NGATUR NGATUR HIDUP ORANG YA NGENTOT, URUS AJA HIDUP LU SENDIRI BANGSAT, UDAH BENER APA KAGAK.**")
+
+
+async def toxic_10(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**GW TAU LU USERBOT TAPI GAUSAH NORAK NGENTOT.**")
+              
+async def toxic_11(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**BACOT NGENTOTTT BAPA LU SURUH RIBUT SAMA GUA.**")
+              
+
+async def toxic_12(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU.**")
+              
+async def toxic_13(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA.**")
+             
+async def toxic_14(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!.**")
+              
+async def toxic_15(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**EHH GOBLOK LU SEMUA RIBUT SAMA GUA SINI NGENTOT!.**")
+             
+async def toxic_16(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**BABI!!KONTOL!!NGENTOT!!!.**")
+              
+async def toxic_17(client, message):
+    if message.forward_from:
+        return
+    await message.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA BHAHAHAHA.**")
