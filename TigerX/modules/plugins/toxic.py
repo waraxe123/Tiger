1f932c14fbc1cfd8fa5a6b8f45e562e0fcef0f2f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-03-21 07:36:19 +0700

    Update toxic.py

--- TigerX/modules/plugins/toxic.py
+++ TigerX/modules/plugins/toxic.py
@@ -5,71 +5,71 @@ from pykillerx.help import add_command_help
 
 @randydev(command("war", cmd) & owner)
 async def war_goblok(client: Client, message: Message):
-    await toxic_1(client, message)
+    await toxic_fixed1(client, message)
 
 @randydev(command("dih", cmd) & owner)
 async def dih_goblok(client: Client, message: Message):
-    await toxic_2(client, message)
+    await toxic_fixed2(client, message)
 
 @randydev(command("gembel", cmd) & owner)
 async def gembel_goblok(client: Client, message: Message):
-    await toxic_3(client, message)
+    await toxic_fixed3(client, message)
 
 @randydev(command("sokab", cmd) & owner)
 async def sokap_goblok(client: Client, message: Message):
-    await toxic_4(client, message)
+    await toxic_fixed4(client, message)
 
 @randydev(command("ded", cmd) & owner)
 async def ded_goblok(client: Client, message: Message):
-    await toxic_5(client, message)
+    await toxic_fixed5(client, message)
 
 @randydev(command("caper", cmd) & owner)
 async def caper_goblok(client: Client, message: Message):
-    await toxic_6(client, message)
+    await toxic_fixed6(client, message)
 
 @randydev(command("lo", cmd) & owner)
 async def lo_goblok(client: Client, message: Message):
-    await toxic_7(client, message)
+    await toxic_fixed7(client, message)
 
 @randydev(command("woi", cmd) & owner)
 async def woi_goblok(client: Client, message: Message):
-    await toxic_8(client, message)
+    await toxic_fixed8(client, message)
 
 @randydev(command("ngatur", cmd) & owner)
 async def ngatur_goblok(client: Client, message: Message):
-    await toxic_9(client, message)
+    await toxic_fixed9(client, message)
 
 @randydev(command("ubot", cmd) & owner)
 async def ubot_goblok(client: Client, message: Message):
-    await toxic_10(client, message)
+    await toxic_fixed10(client, message)
 
 @randydev(command("d", cmd) & owner)
 async def d_goblok(client: Client, message: Message):
-    await toxic_11(client, message)
+    await toxic_fixed11(client, message)
 
 @randydev(command("e", cmd) & owner)
 async def e_goblok(client: Client, message: Message):
-    await toxic_12(client, message)
+    await toxic_fixed12(client, message)
 
 @randydev(command("f", cmd) & owner)
 async def f_goblok(client: Client, message: Message):
-    await toxic_13(client, message)
+    await toxic_fixed13(client, message)
 
 @randydev(command("i", cmd) & owner)
 async def i_goblok(client: Client, message: Message):
-    await toxic_14(client, message)
+    await toxic_fixed14(client, message)
 
 @randydev(command("q", cmd) & owner)
 async def q_goblok(client: Client, message: Message):
-    await toxic_15(client, message)
+    await toxic_fixed15(client, message)
 
 @randydev(command("t", cmd) & owner)
 async def t_goblok(client: Client, message: Message):
-    await toxic_16(client, message)
+    await toxic_fixed16(client, message)
 
 @randydev(command("u", cmd) & owner)
 async def u_goblok(client: Client, message: Message):
-    await toxic_17(client, message)
+    await toxic_fixed17(client, message)
 
 add_command_help(
     "toxic",
