41d06c32e8ceffd1a83e30be5609f9c798e8de9f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-01 20:58:15 +0700

    Create take.py

---
+++ TigerX/lib/take.py
@@ -0,0 +1,14 @@
+from TigerX.lib import *
+from TigerX import * 
+
+async def take_corret(client, message):
+    caption = message.text.split(None, 1)[1] if message else None
+    replied = message.reply_to_message
+    take_this = replied.copy(m.chat.id, caption=caption) if replied else None
+    if not caption or not replied:
+        await message.reply_text("please reply to a message")
+    else:
+        try:
+            await take_this
+        except Exception e:
+            await message.reply_text(str(e))
