3bff4da12d79cfd2f7e9926dc3bd05b1406288a3
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-01 22:43:18 +0700

    fix

--- TigerX/lib/sys.py
+++ TigerX/lib/sys.py
@@ -1,14 +1,20 @@
 from TigerX import *
 from TigerX.lib.cbn import make_carbon
 from TigerX.lib.developer import shell_exec
+import os
 
 async def sysinfo(c, m):
     chat_id = m.chat.id
     pro = await m.reply_text("`Prossing....`")
     try:
         sysinfo = (await shell_exec("neofetch | sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g'"))[0]
-        carbon = await make_carbon(sysinfo)
+    except Exception:
+        await os.system("apt-get install -y neofetch")
+        return
+    carbon = await make_carbon(sysinfo)
+    try:
         await c.send_photo(chat_id, carbon, caption=f"Carbonised by {c.me.mention}")
         await pro.delete()
-    except Exception:
-        pass
+    except Exception as e:
+        await pro.edit_text(str(e))
+        return 
