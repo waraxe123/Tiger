6d8d634e05d22359de09eafd9e3cd0ad69f8209e
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-01 23:02:41 +0700

    Update sys.py

--- TigerX/lib/sys.py
+++ TigerX/lib/sys.py
@@ -2,14 +2,19 @@ from TigerX import *
 from TigerX.lib.cbn import make_carbon
 from TigerX.lib.developer import shell_exec
 import os
+import asyncio
 
 async def sysinfo(c, m):
     chat_id = m.chat.id
     pro = await m.reply_text("`Prossing....`")
+    sleep_seconds = asyncio.sleep(5) if asyncio.sleep(2) else None
+    install_system = os.system("apt-get install -y neofetch") if os else None
     try:
+        await install_system
+        await sleep_seconds
         sysinfo = (await shell_exec("neofetch | sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g'"))[0]
     except Exception:
-        await os.system("apt-get install -y neofetch")
+        pass
         return
     carbon = await make_carbon(sysinfo)
     try:
