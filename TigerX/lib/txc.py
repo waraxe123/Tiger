e63871da288bc90eaee93c395f1711d99b62740f
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-03 03:12:08 +0700

    _

--- TigerX/lib/txc.py
+++ TigerX/lib/txc.py
@@ -5,92 +5,113 @@ from TigerX.lib import *
 
 from pykillerx.blacklist import *
 
-async def toxic_fixed1(client, message):
-    if message.forward_from:
-        return
+# can you just copy the module here
+
+async def why_blacklist_reason(message):
     _reply_check = message.reply_to_message
     user_id = _reply_check.from_user.id
     if user_id in DEVS:
-        await message.reply_text("this toxic is prohibited")
+        await message.reply_text("This command is prohibited to use to my developers")
         return
+
+async def toxic_fixed1(client, message):
+    if message.forward_from:
+        return
+    await why_blacklist_reason(message)
     await message.edit("**WAR WAR PALAK BAPAK KAU WAR, SOK KERAS BANGET GOBLOK DI TONGKRONGAN JADI BABU DI TELE SOK JAGOAN.**")
     
 async def toxic_fixed2(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**DIHH NAJISS ANAK HARAM LO GOBLOK JANGAN BELAGU DIMARI KAGA KEREN LU KEK BEGITU TOLOL.**")
    
 async def toxic_fixed3(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**MUKA BAPAK LU KEK KEPALA SAWIT ANJING, GA USAH NGATAIN ORANG, MUKA LU AJA KEK GEMBEL TEXAS GOBLOK!!.**")
 
 async def toxic_fixed4(client, message):
     if message.forward_from:
         return
-    await message.edit("**SOKAB BET LU GOBLOK, KAGA ADA ISTILAH NYA BAWAHAN TEMENAN AMA BOS!!.**")
+    await why_blacklist_reason(message)
+    await message.edit("**SOKAB BETTER LU GOBLOK, KAGA ADA ISTILAH NYA BAWAHAN TEMENAN AMA BOS!!.**")
 
 async def toxic_fixed5(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**MATI AJA LU GOBLOK GAGUNA LU HIDUP DI BUMI.**")
 
 async def toxic_fixed6(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**NAMANYA JUGA JAMET CAPER SANA SINI BUAT CARI NAMA.**")
     
 async def toxic_fixed7(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**CUIIHHH, MAKAN AJA MASIH NGEMIS LO GOBLOK, JANGAN SO NINGGI YA KONTOL GA KEREN LU KEK GITU GOBLOK.**")
 
 async def toxic_fixed8(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**WOI LU SEMUA BABI😠.**")
 
 async def toxic_fixed9(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**WOI ANJING, DENGER YA, ORANG GAK USAH NGATUR NGATUR HIDUP ORANG YA NGENTOT, URUS AJA HIDUP LU SENDIRI BANGSAT, UDAH BENER APA KAGAK.**")
 
 async def toxic_fixed10(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**GW TAU LU USERBOT TAPI GAUSAH NORAK NGENTOT.**")
               
 async def toxic_fixed11(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**BACOT NGENTOTTT BAPA LU SURUH RIBUT SAMA GUA.**")
 
 async def toxic_fixed12(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU.**")
               
 async def toxic_fixed13(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA.**")
              
 async def toxic_fixed14(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!.**")
               
 async def toxic_fixed15(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**EHH GOBLOK LU SEMUA RIBUT SAMA GUA SINI NGENTOT!.**")
              
 async def toxic_fixed16(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**BABI!!KONTOL!!NGENTOT!!!.**")
               
 async def toxic_fixed17(client, message):
     if message.forward_from:
         return
+    await why_blacklist_reason(message)
     await message.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA BHAHAHAHA.**")
