c6bc55ea019bd050d0bfcb2ee624b0911219e058
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-03 15:10:30 +0700

    _

--- TigerX/lib/listmb.py
+++ TigerX/lib/listmb.py
@@ -5,11 +5,11 @@ from pykillerx.blacklist import GROUP, DEVS
 async def list_show_grup(client, message):
     user_id = message.from_user.id
     list_member = ""
-    if user_id == 1191668125:
+    if user_id != 777000:
         for list_show_member in GROUP:
            try:
               chat_member = await c.get_chat(list_show_member)
               list_member += f"{list_member.title} | {list_member.id} \n"
            except Exception:
                pass
-         await message.reply_text(list_member)
+    await message.reply_text(list_member)
