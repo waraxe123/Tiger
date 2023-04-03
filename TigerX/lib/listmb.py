bc07c645ea97c733a3ca8ecc43c7f6a5816c2df1
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-03 15:19:18 +0700

    _

--- TigerX/lib/listmb.py
+++ TigerX/lib/listmb.py
@@ -3,7 +3,7 @@ from TigerX.lib import *
 from pykillerx.blacklist import GROUP, DEVS
 
 async def list_show_grup(client, message):
-    user_id = message.from_user.id
+    user_id = message.reply_to_message.from_user.id if message.reply_to_message.from_user else None
     list_member = ""
     if user_id != 777000:
         await message.reply_text("banned personal telegram")
