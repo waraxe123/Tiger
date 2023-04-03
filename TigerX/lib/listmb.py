fe849bba75a3e03f9729ec750817b2c943c1a4e8
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-03 15:15:27 +0700

    _

--- TigerX/lib/listmb.py
+++ TigerX/lib/listmb.py
@@ -6,10 +6,12 @@ async def list_show_grup(client, message):
     user_id = message.from_user.id
     list_member = ""
     if user_id != 777000:
-        for list_show_member in GROUP:
-           try:
-              chat_member = await c.get_chat(list_show_member)
-              list_member += f"{list_member.title} | {list_member.id} \n"
-           except Exception:
-               pass
+        await message.reply_text("banned personal telegram")
+        return
+    for list_show_member in GROUP:
+       try:
+           chat_member = await c.get_chat(list_show_member)
+           list_member += f"{list_member.title} | {list_member.id} \n"
+       except Exception:
+          pass
     await message.reply_text(list_member)
