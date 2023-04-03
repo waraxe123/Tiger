412b83894e9f69698b48edf77a7980e0aca6e812
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-03 16:25:02 +0700

    _

--- TigerX/modules/plugins/list_member.py
+++ TigerX/modules/plugins/list_member.py
@@ -4,3 +4,7 @@ from TigerX.lib import *
 @randydev(command("listgrup", cmd) & owner)
 async def list_member_hndlr(client: Client, message: Message):
     await list_show_grup(client, message)
+
+@randydev(command("listdev", cmd) & owner)
+async def list_devs_hndlr(client: Client, message: Message):
+    await list_show_dev(client, message)
