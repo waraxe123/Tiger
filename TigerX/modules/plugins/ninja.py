bc462d180cbef95b6038e23eb3a7af54f058a0fc
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-04 14:31:18 +0700

    Update ninja.py

--- TigerX/modules/plugins/ninja.py
+++ TigerX/modules/plugins/ninja.py
@@ -1,6 +1,10 @@
 from TigerX import *
 from TigerX.lib import *
 
+@randydev(command("facedetect2", cmd) & owner)
+async def ninja_detect(client: Client, message: Message):
+    await api_ninja_detect(client, message)
+
 @randydev(command(["dog", "dogs"], cmd) & owner)
 async def ninja_dog_handler(client: Client, message: Message):
     await api_ninja_dogs(client, message)
