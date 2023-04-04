6d5976480ff5525a77536e74cf49b481d2918263
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-04 15:52:42 +0700

    fixed

--- TigerX/lib/api_random.py
+++ TigerX/lib/api_random.py
@@ -19,7 +19,7 @@ async def api_ceo_dog(client, message):
         try:
             photo_dog_url = data_ceo["message"]
         except Exception as e:
-            await pro.edit_text(f"Error requests: {e}")
+            await ran.edit_text(f"Error requests: {e}")
             return 
         await client.send_photo(message.chat.id, photo=photo_dog_url, reply_to_message_id=message.id)
     else:
@@ -30,13 +30,16 @@ async def api_ceo_dog(client, message):
         pass
 
 async def api_big_cat(client, message):
+    ran = await message.reply_text("<code>Uploading.......</code>")
     API_BIG_CAT = "https://randombig.cat/roar.json"
     response = requests.get(API_BIG_CAT)
     if response.status_code == 200:
         data_cat = response.json()
-        
-        photo_cat_url = data_cat[0]["url"]
-        ran = await message.reply_text("<code>Uploading.......</code>")
+        try:
+            photo_cat_url = data_cat["url"]
+        except Exception as e:
+            await ran.edit_text(f"Error requests: {e}")
+            return
         await client.send_photo(message.chat.id, photo=photo_cat_url, reply_to_message_id=message.id)
     else:
         await ran.edit_text("Sorry, there was an error processing your request. Please try again later")
@@ -46,13 +49,16 @@ async def api_big_cat(client, message):
         pass
 
 async def api_ceo_dog2(client, message):
+    ran = await message.reply_text("<code>Uploading.......</code>")
     API_CEO_DOG2 = "https://random.dog/woof.json"
     response = requests.get(API_CEO_DOG2)
     if response.status_code == 200:
         data_dog2 = response.json()
-        
-        photo_dog2_url = data_dog2[0]["url"]
-        ran = await message.reply_text("<code>Uploading.......</code>")
+        try:
+            photo_dog2_url = data_dog2["url"]
+        except Exception as e:
+            await ran.edit_text(f"Error requests: {e}")
+            return
         await client.send_photo(message.chat.id, photo=photo_dog2_url, reply_to_message_id=message.id)
     else:
         await ran.edit_text("Sorry, there was an error processing your request. Please try again later")
