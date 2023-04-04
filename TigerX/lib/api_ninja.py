cc94eb0f23f95d299698791a6fcc25a29cbc2eca
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-04 13:37:11 +0700

    _

--- TigerX/lib/api_ninja.py
+++ TigerX/lib/api_ninja.py
@@ -20,11 +20,11 @@ async def api_ninja_dogs(client, message):
     pro = await message.reply_text("<code>Prossing.......</code>")
     term_param = message.text.split(None, 1)[1] if len(message.command) > 1 else None
     if not term_param:
-        await message.reply_text("Example: <code>+dog golden retriever</code>")
+        await pro.edit_text("Example: <code>+dog golden retriever</code>")
         return
 
     if not API_NINJA_DOG:
-        await message.reply_text("Missing api key: <code>API_NINJA_DOG</code>")
+        await pro.edit_text("Missing api key: <code>API_NINJA_DOG</code>")
         return
 
     search_image = term_param
@@ -36,7 +36,7 @@ async def api_ninja_dogs(client, message):
     send_image_url = response[0]["image_link"]
     real_name = response[0]["name"]
     try:
-        this_ninja = await pro.edit("<code>Uploading ninja dog.....</code>")
+        this_ninja = await pro.edit_text("<code>Uploading ninja dog.....</code>")
         await client.send_photo(message.chat.id, photo=send_image_url, caption=real_name, reply_to_message_id=message.id)
     except Exception as e:
         await pro.edit_text(f"No results found: {e}")
