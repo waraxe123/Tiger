9d288a1cf07c5c53765e38cf444cb657db3e9801
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-04 12:19:35 +0700

    Create ninja.py

---
+++ TigerX/modules/plugins/ninja.py
@@ -0,0 +1,6 @@
+from TigerX import *
+from TigerX.lib import *
+
+@randydev(command(["dog", "dogs"], cmd) & owner)
+async def ninja_dog_handler(client: Client, message: Message):
+    await api_ninja_dogs(client, message)
