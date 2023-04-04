466a6bde4fdba8f30ea0bb8dad47fa628853d0ab
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-04 14:18:16 +0700

    fixed added

--- TigerX/lib/api_ninja.py
+++ TigerX/lib/api_ninja.py
@@ -14,33 +14,61 @@ import requests
 
 # you can get this api key : https://api-ninjas.com/profile
 
-API_NINJA_DOG = "Pyp4hM0TJqb+ZZUpRBUSTQ==B1VFKxPVKSWbCVRZ" # don't share this,
+API_NINJA_NEW = "Pyp4hM0TJqb+ZZUpRBUSTQ==B1VFKxPVKSWbCVRZ" # don't share this,
 
 async def api_ninja_dogs(client, message):
-    pro = await message.reply_text("<code>Prossing.......</code>")
     term_param = message.text.split(None, 1)[1] if len(message.command) > 1 else None
     if not term_param:
-        await pro.edit_text("Example: <code>+dog golden retriever</code>")
+        await message.reply_text("Example: <code>+dog golden retriever</code>")
         return
 
-    if not API_NINJA_DOG:
-        await pro.edit_text("Missing api key: <code>API_NINJA_DOG</code>")
+    if not API_NINJA_NEW:
+        await message.reply_text("Missing api key: <code>API_NINJA_DOG</code>")
         return
 
     search_image = term_param
     api_url = f"https://api.api-ninjas.com/v1/dogs?name={search_image}"
 
-    headers = {"X-Api-Key": API_NINJA_DOG}
+    headers = {"X-Api-Key": API_NINJA_NEW}
     
     response = requests.get(api_url, headers=headers).json()
     send_image_url = response[0]["image_link"]
     real_name = response[0]["name"]
     try:
-        this_ninja = await pro.edit_text("<code>Uploading ninja dog.....</code>")
         await client.send_photo(message.chat.id, photo=send_image_url, caption=real_name, reply_to_message_id=message.id)
     except Exception as e:
-        await pro.edit_text(f"No results found: {e}")
-    try:
-        await this_ninja.delete()
-    except Exception:
-        pass
+        await client.send_message(message.chat.id, f"No results found: {e}", reply_to_message_id=message.id)
+
+
+async def api_ninja_detect(client, message):
+    if not message.reply_to_message or not message.reply_to_message.photo:
+        await message.reply_text("Please reply to a message with a photo.")
+        return
+
+    file_id = message.reply_to_message.photo.file_id
+    photo_path = await client.download_media(file_id)
+
+    api_url = "https://api.api-ninjas.com/v1/facedetect"
+    with open(photo_path, "rb") as f:
+        files = {"image": f}
+        headers = {"X-Api-Key": API_NINJA_NEW}
+
+        response = requests.post(api_url, files=files, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            annotated_url = data["annotated_image_link"]
+
+            await client.send_photo(
+                message.chat.id,
+                photo=annotated_url,
+                reply_to_message_id=message.id
+            )
+
+        else:
+            await message.reply_text(
+                "Sorry, there was an error processing your request. Please try again later."
+            )
+
+    os.remove(photo_path)
