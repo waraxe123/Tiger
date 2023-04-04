8494b423c84a2ca6112e15ef588aad0c228eb2a2
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-05 01:00:28 +0700

    _

--- TigerX/lib/api_random.py
+++ TigerX/lib/api_random.py
@@ -9,6 +9,7 @@
 # Developer Credits: @xtsea
 
 import requests
+from random import choice
 
 async def api_ceo_dog(client, message):
     ran = await message.reply_text("<code>Uploading.......</code>")
@@ -112,3 +113,25 @@ async def api_animechan_new(client, message):
     else:
         await ran.edit_text("Sorry, there was an error processing your request. Please try again later")
 
+
+async def api_waifu_main(client, message):
+    ran = await message.reply_text("<code>Processing.......</code>")
+    LIST_SFW_JPG = ["neko", "waifu", "megumin"]
+    waifu_link = "https"
+    waifu_api = "api.waifu.pics"
+    waifu_types = "sfw"
+    waifu_category = choice(LIST_SFW_JPG)
+    waifu_param = f"{waifu_link}://{waifu_api}/{waifu_types}/{waifu_category}"
+    if response = requests.get(waifu_param)
+        data_waifu = response.json()
+        try:
+            waifu_image_url = data_waifu["url"]
+        except Exception as e:
+            await ran.edit_text(f"Error request {e}")
+            return
+        if waifu_image_url:
+            await client.send_photo(message.chat.id, photo=waifu_image_url, reply_to_message_id=message.id)
+        else:
+            await ran.edit_text("Not founds animechan")
+    else:
+        await ran.edit_text("Sorry, there was an error processing your request. Please try again later")
