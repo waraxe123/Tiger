74b4b4941009c74f3c8d8c5d927154e34ae63854
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-04 15:36:08 +0700

    _

--- TigerX/modules/plugins/image_random.py
+++ TigerX/modules/plugins/image_random.py
@@ -7,8 +7,8 @@ async def cat_image_fixed(client: Client, message: Message):
 
 @randydev(command("dog2", cmd) & owner)
 async def image_fixed(client: Client, message: Message):
-    await api_ceo_dog(client, message):
+    await api_ceo_dog(client, message)
 
 @randydev(command("dog3", cmd) & owner)
 async def image_fixed_2(client: Client, message: Message):
-    await api_ceo_dog2(client, message):
+    await api_ceo_dog2(client, message)
