a74253b95089d9427b98ca702d9a531b6c317751
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-05 22:25:07 +0700

    _

--- TigerX/lib/fry.py
+++ TigerX/lib/fry.py
@@ -10,29 +10,30 @@ from pyrogram.types import Message
 
 async def frybot(client, message):
     reply = message.reply_to_message
-    if not (reply and reply.photo and reply.document):
+    if not (reply and (reply.photo or reply.sticker)):
         await message.reply_text("`Reply to a photo/sticker`")
         return
-
-    eris = await message.reply_text("`frying...`")
+    
+    file_id = reply.photo.file_id if reply.photo else reply.sticker.file_id
+    file_path = await client.download_media(file_id)
     CHAT = await client.resolve_peer("image_deepfrybot")
     if args := message.text.split(None, 1)[1]:
         args = args.lstrip()
     try:
-        async with client.conversation(CHAT, total_timeout=15) as conv:
-            file = await conv.send_media(reply)
-            if args and args.isdigit():
-                await sleep(2)
-                await conv.send_message(f"/deepfry {args}", reply_to_message_id=file.id)
-            response = await conv.get_response()
-            await asyncio.gather(
-                reply.reply(file=response.media),
-                eris.delete(),
-                conv.mark_read(),
-            )
-    except UserIsBlocked:
-        await eris.edit("`Sar, plox Unblock @image_deepfrybot`")
-    except TimeoutError:
-        await eris.edit("`Bot didn't respond in time..`")
-    except Exception as exc:
-        await eris.edit(f"**Error:** `{exc}`")
+        await client.send_message(CHAT, f"/deepfry {args}")
+        await sleep(3)
+        send = client.send_photo(CHAT, photo=file_path) or client.send_document(CHAT, file_path)
+        if send:
+            async for x in client.search_messages(CHAT, limit=1):
+                file_id = x.photo.file_id
+                send_param = await client.download_media(file_id)
+                send_other = client.send_photo(message.chat.id, photo=send_param) or client.send_document(message.chat.id, send_param)
+                if send_other:
+                    await send_other
+                    break
+                else:
+                    await client.send_message(message.chat.id, "Failed Error", reply_to_message_id=message.id)
+        else:
+            await client.send_message(message.chat.id, "Failed Error", reply_to_message_id=message.id)
+    except Exception as e:
+        await message.reply_text(f"Error request {e}")
