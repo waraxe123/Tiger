3911dec33ed1815f43052d8f3c3cba2a1b169e1b
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-06 18:15:41 +0700

    _

--- TigerX/lib/tiktok.py
+++ TigerX/lib/tiktok.py
@@ -35,29 +35,22 @@ async def tiktok_downloader(client, message):
             await ran.edit_text(f"Error request {e}")
             return
         
-        video_responses = []
         for video_url in video_urls:
             video_response = requests.get(video_url)
-            video_responses.append(video_response)
-        music_responses = []
         for music_url in music_urls:
             music_response = requests.get(music_url)
-            music_responses.append(music_response)
-        
-        full_video = all(video_response.ok for video_response in video_responses)
-        full_music = all(music_response.ok for music_response in music_responses)
         
         if music_urls and video_urls:
-            if full_video:
+            if video_response:
                 send_video_file_path = "tigerx_userbot.mp4"
                 with open(send_video_file_path, "wb") as f:
-                    f.write(video_responses[0].content)
+                    f.write(video_response.content)
                 await client.send_video(message.chat.id, video=send_video_file_path, reply_to_message_id=message.id)
                 os.remove(send_video_file_path)
-            elif full_music:
+            elif music_response:
                 send_audio_file_path = "tigerx_userbot.mp3"
                 with open(send_audio_file_path, "wb") as f:
-                    f.write(music_responses[0].content)
+                    f.write(music_response.content)
                 await client.send_audio(message.chat.id, audio=send_audio_file_path, reply_to_message_id=message.id)
                 os.remove(send_audio_file_path)
             else:
