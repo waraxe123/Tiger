3a244e75c2fef398cb17026e6df07c4b0f5339ee
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-06 16:55:37 +0700

    fixed

--- TigerX/lib/tiktok.py
+++ TigerX/lib/tiktok.py
@@ -14,8 +14,8 @@ import requests
 import os
 
 async def tiktok_downloader(client, message):
-    ran = await message.reply_text("<code>Processing.....</code")
-    link = message.text.split(None, 1[1] if len(message.command) !=1 else None
+    ran = await message.reply_text("<code>Processing.....</code>")
+    link = message.text.split(None, 1)[1] if len(message.command) !=1 else None
     if not link:
         await ran.edit_text("please for example the TikTok link here")
         return
@@ -26,32 +26,37 @@ async def tiktok_downloader(client, message):
     headers = {"X-RapidAPI-Key": "ce36c261f1mshb4a0a55aaca548ep12c9f3jsn3d6761cb63fb", "X-RapidAPI-Host": "tiktok-full-info-without-watermark.p.rapidapi.com"}
 
     response = requests.get(url, headers=headers, params=querystring)
-    data_response = response.json()
     if response.status_code == 200:
         data_response = response.json()
         try:
             music_urls = data_response["music"]
             video_urls = data_response["video"]
         except Exception as e:
-            await ran.edit_text("Error request {e}")
+            await ran.edit_text(f"Error request {e}")
             return
+        
+        video_responses = []
         for video_url in video_urls:
-            video_response = requests.get(video_url)
+            video_responses.append(requests.get(video_url))
+        music_responses = []
         for music_url in music_urls:
-            music_response = requests.get(music_url)
+            music_responses.append(requests.get(music_url))
+        
         if music_urls and video_urls:
-            if video_response:
+            if all(response.ok for response in video_responses):
                 send_video_file_path = "tigerx_userbot.mp4"
                 with open(send_video_file_path, "wb") as f:
-                    f.write(music_response.content)
-                await client.send_video(message.chat.id, video=send_video_file_path, reply_to_message_id=message.id)
-                os.remove(send_music_file_path)
-            elif music_response:
+                    for response in video_responses:
+                        f.write(response.content)
+                await client.send_video(message.chat.id, video=send_video_file_path, reply_to_message_id=message.message_id)
+                os.remove(send_video_file_path)
+            elif all(response.ok for response in music_responses):
                 send_audio_file_path = "tigerx_userbot.mp3"
                 with open(send_audio_file_path, "wb") as f:
-                    f.write(music_response.content)
-                await client.send_audio(message.chat.id, audio=send_audio_file_path, reply_to_message_id=message.id)
-                os.remove(send_music_file_path)
+                    for response in music_responses:
+                        f.write(response.content)
+                await client.send_audio(message.chat.id, audio=send_audio_file_path, reply_to_message_id=message.message_id)
+                os.remove(send_audio_file_path)
             else:
                 await ran.edit_text("Error please try again")
         else:
