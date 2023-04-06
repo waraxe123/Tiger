75f9136da30a07a88f120daf06e56b41a26f4e12
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-07 02:49:14 +0700

    _

--- TigerX/modules/plugins/new_tiktok.py
+++ TigerX/modules/plugins/new_tiktok.py
@@ -6,3 +6,7 @@ from pykillerx.help import add_command_help
 @randydev(command(["tt", "tiktok"], cmd) & owner)
 async def tiktok_handler(client: Client, message: Message):
     await tiktok_downloader(client, message)
+
+@randydev(command(["tt2", "tiktok2"], cmd) & owner)
+async def tiktok_handler2(client: Client, message: Message):
+    await tiktok_downloader_2(client, message)
