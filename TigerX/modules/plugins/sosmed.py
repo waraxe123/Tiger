7c5868e2c891d94236e3cb80002c043533889ed3
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-08 02:21:22 +0700

    Create sosmed.py

---
+++ TigerX/modules/plugins/sosmed.py
@@ -0,0 +1,14 @@
+from TigerX import *
+from TigerX.lib import *
+
+from pykillerx.help import *
+
+add_command_help(
+    "sosmed",
+    [
+        ["igdl", "to photo or video using the API Instagram."],
+        ["fbdl", "to video using the api Facebook"],
+        ["tt", "to ask questions using the api TikTok"],
+
+    ],
+)
