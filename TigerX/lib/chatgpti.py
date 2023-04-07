492b168f13b39346dc2d6597b15a3ef391ca03bf
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-07 20:12:40 +0700

    _

--- TigerX/lib/chatgpti.py
+++ TigerX/lib/chatgpti.py
@@ -34,7 +34,7 @@ async def new_model_chatgpt(client, message):
         await ran.edit_text("question ask this chagpt")
         return
     url = "https://openai80.p.rapidapi.com/completions"
-    payload = {"model": "text-davinci-003", "prompt": asked, "max_tokens": 7, "temperature": 0, "top_p": 1, "n": 1, "stream": False, "logprobs": None, "stop": None}
+    payload = {"model": "text-davinci-003", "prompt": asked, "max_tokens": 200, "temperature": 0, "top_p": 1, "n": 1, "stream": False, "logprobs": None, "stop": None}
     headers = {"content-type": "application/json", f"X-RapidAPI-Key": APIKEY, "X-RapidAPI-Host": "openai80.p.rapidapi.com"}
     response = requests.request("POST", url, json=payload, headers=headers)
     if not APIKEY:
