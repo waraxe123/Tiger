76f6e965ea2f6182cda895c7b4be5ef7c3276808
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-08 02:16:39 +0700

    _

--- TigerX/modules/plugins/chatgpt.py
+++ TigerX/modules/plugins/chatgpt.py
@@ -24,14 +24,17 @@ async def chatgpt_rapi_cmd(client: Client, message: Message):
 async def chatgpt_rapi_turbo_cmd(client: Client, message: Message):
     await new_chatgpt_turbo(client, message)
 
-@randydev(command(["aimage", "dalle"], cmd) & owner)
+@randydev(command(["dalle"], cmd) & owner)
 async def chatgpt_image_cmd(c: Client, m: Message):
     await chatpgt_image_generator(c, m)
 
 add_command_help(
-    "chatgpt",
+    "openai",
     [
-        [f"ask [question]", "to ask questions using the API."],
-        [f"aimage [question]", "to chatgpt image generator using the API."],
+        ["askd [question]", "to ask questions using the API chagpt openai."],
+        ["askd2 [question]", "to ask questions using the api chatgpt openai v2"],
+        ["askt [question]", "to ask questions using the api chagpt turbo"],
+        ["dalle [question]", "to chatgpt image generator using the API."],
+
     ],
 )
