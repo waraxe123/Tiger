4b8801aa77009540085228470cef93701da1effe
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-09 00:55:23 +0700

    _

--- TigerX/lib/chatgpti.py
+++ TigerX/lib/chatgpti.py
@@ -22,27 +22,12 @@ from TigerX import OPENAI_API
 from TigerX import *
 from TigerX.lib import *
 
+from pykillerx.openai import PayLoadHeaders
+
 
 # model text-davinci-003
 # using rapidapi.com
 
-class PayLoadHeaders:
-    def __init__(self, model, prompt, api_key):
-        self.payload = {
-            "model": model,
-            "prompt": prompt,
-            "max_tokens": 200,
-            "temperature": 0,
-            "top_p": 1,
-            "stream": False,
-            "logprobs": None,
-            "stop": None
-        }
-        self.headers = {
-            "content-type": "application/json",
-            "X-RapidAPI-Key": apikey,
-            "X-RapidAPI-Host": "openai80.p.rapidapi.com"
-        }
 
 async def new_model_chatgpt(client, message):
     ran = await message.reply_text("<code>Processing....</code>")
