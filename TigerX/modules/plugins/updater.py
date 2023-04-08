fac9010d1e86168b70851bbddf87b856cb77fc5e
xtsea <90479255+xtsea@users.noreply.github.com>
2023-04-08 16:18:15 +0700

    Update updater.py

--- TigerX/modules/plugins/updater.py
+++ TigerX/modules/plugins/updater.py
@@ -2,7 +2,7 @@ from TigerX import *
 from TigerX.lib import *
 
 @randydev(command("update", cmd) & owner)
-async def update_handler(client: Client, message: Message):
+async def update_handler_fixed(client: Client, message: Message):
     await upstream(client, message)
 
 @randydev(command("goupdate", cmd) & owner)
